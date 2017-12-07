1cbb460d144974be49414d0ad6388a2ea34fb76a
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2017-12-07 17:08:18 +1100

    feat(day1): Extended solution

--- src/Days/Day1.hs
+++ src/Days/Day1.hs
@@ -4,9 +4,14 @@ day1 :: IO ()
 day1 = do
   input <- readFile "input/day1.in"
   let numbers = map (read . return) input
-  let answer = sum $ fst <$> filter (uncurry (==)) (zipAdj numbers)
+  let answer = checksum 1 numbers
+  let answerExtended = checksum (length numbers `div` 2) numbers
   print answer
+  print answerExtended
 
-zipAdj :: [a] -> [(a, a)]
-zipAdj (x:xs) = zip (x:xs) (xs ++ [x])
-zipAdj []     = []
+checksum :: Int -> [Int] -> Int
+checksum offset numbers = sum $ fst <$> filter (uncurry (==)) zippedNumbers
+  where zippedNumbers = zipAdjOffset offset numbers
+
+zipAdjOffset :: Int -> [a] -> [(a, a)]
+zipAdjOffset offset xs = zip xs (drop offset (xs ++ xs))
